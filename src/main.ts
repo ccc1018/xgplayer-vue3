aa4d37c8b11c713088a384a920648e688bda645f
ccc1018 <2071138578@qq.com>
2025-07-12 21:13:45 +0800

    feat: 🚀 feat/章节和字幕

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,16 @@ import './style.css';
 import App from './App.vue';
 import ArcoVue from '@arco-design/web-vue';
 import '@arco-design/web-vue/dist/arco.css';
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
+import { createPinia } from 'pinia';
 import router from './router';
 if (import.meta.env.MODE === 'development') {
   import('./mock');
 }
 const app = createApp(App);
-
+const pinia = createPinia();
 app.use(ArcoVue);
 app.use(router);
+app.use(pinia);
+pinia.use(piniaPluginPersistedstate);
 app.mount('#app');
